b80443c8d0bc841aeffa4f0ea24d1c58c4c22f39
thomas bongiorno <tbongiorno1@gmail.com>
2016-09-01 11:41:28 -0400

    some question updates

--- app.js
+++ app.js
@@ -33,10 +33,10 @@ class Question {
 }
 
 
-const questionOne = new Question('Which house do you belong to?', 'Bad Guys House', 'Good Guys House', 'Other Guys House', 'The house no one cares about', 'http://i.imgur.com/Y1anZ7w.jpg', 'one');
+const questionOne = new Question('You are walking through the woods. You see a young boy all alone holding a sign for donations of coins. As you begin to approach you have a weird feeling that something is wrong. What do you do?', 'Walk over and give the boy some coins. Youre sure its nothing', 'Run like the wind! Who knows what kind of scam this bandit is pulling!', 'Other Guys House', 'The house no one cares about', 'http://i.imgur.com/bFzPiBP.jpg', 'one');
 questionOne.render();
 
-const questionTwo = new Question('Which house do you belong to?', 'Bad Guys House', 'Good Guys House', 'Other Guys House', 'The house no one cares about', 'http://i.imgur.com/Y1anZ7w.jpg', 'two');
+const questionTwo = new Question('While doing your chores in the castle you over-hear a juicey bit of information about the queen. What do you do with this information?', 'Run directly to Lord Varus to win his favor', 'Sell the information to Littlefinger', 'Blackmail Cersei directly', 'Nothing. You would never get yourself involved with this type of risky business.', 'http://i.imgur.com/NAPMUFQ.jpg', 'two');
 questionTwo.render();
 
 const questionThird = new Question('Which house do you belong to?', 'Bad Guys House', 'Good Guys House', 'Other Guys House', 'The house no one cares about', 'http://i.imgur.com/Y1anZ7w.jpg', 'three');
